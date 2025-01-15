3d08eeffc1193835e9b71422e08818b13d46add9
Diego Alfonzo <149984303+dalfonzo21@users.noreply.github.com>
2025-01-15 14:41:43 -0500

    Update OperationsTest.java

--- TALLER7/src/test/java/com/espol/poo/taller7/OperationsTest.java
+++ TALLER7/src/test/java/com/espol/poo/taller7/OperationsTest.java
@@ -101,31 +101,31 @@ public class OperationsTest {
     }
     @Test
     @DisplayName("Test que verifica que la solución de una fórmula de suma es correcta")
-    void testSolveAddition() {
-        String formula = "3+5";
-        String result = Operations.Solve(formula);
-        assertEquals("3+5=8", result, "La solución de 3+5 debe ser 8.");
+    void testSolveAddition(String formula) {
+        formula = "3+5";
+        String result1 = Operations.Solve(formula);
+        assertEquals("3+5=8", result1, "La solución de 3+5 debe ser 8.");
     }
 
     @Test
     @DisplayName("Test que verifica que la solución de una fórmula de resta es correcta")
-    void testSolveSubtraction() {
-        String formula = "8-3";
-        String result = Operations.Solve(formula);
-        assertEquals("8-3=5", result, "La solución de 8-3 debe ser 5.");
+    void testSolveSubtraction(String formula) {
+        formula = "8-3";
+        String result1 = Operations.Solve(formula);
+        assertEquals("8-3=5", result1, "La solución de 8-3 debe ser 5.");
     }
     @Test
     @DisplayName("Test que verifica que la solución de una fórmula de multiplicación es correcta")
-    void testSolveMultiplication() {
-        String formula = "4*6";
+    void testSolveMultiplication(String formula) {
+        formula = "4*6";
         String result1 = Operations.Solve(formula);
         assertEquals("4*6=24", result1, "La solución de 4*6 debe ser 24.");
     }
 
     @Test
     @DisplayName("Test que verifica que la solución de una fórmula de división es correcta")
-    void testSolveDivision() {
-        String formula = "8/2";
+    void testSolveDivision(String formula) {
+        formula = "8/2";
         String result1 = Operations.Solve(formula);
         assertEquals("8/2=4", result1, "La solución de 8/2 debe ser 4.");
     }
