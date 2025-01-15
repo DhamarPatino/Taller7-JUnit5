5677b1274947e2245001b803eecfe9c76bb11723
Diego Alfonzo <149984303+dalfonzo21@users.noreply.github.com>
2025-01-15 13:33:38 -0500

    prueba

--- TALLER7/src/main/java/com/espol/poo/taller7/CalculatorSMP.java
+++ TALLER7/src/main/java/com/espol/poo/taller7/CalculatorSMP.java
@@ -15,7 +15,7 @@ public class CalculatorSMP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        System.out.println("Hola mundo");
         String question = Operations.MakeFormula();
         System.out.println(question);
         String ret = Operations.Solve(question);
